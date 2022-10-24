ae0f3f306b8a73f95d4fb147d07e8a49ae76e60b
Grushaas <grusha_as@outlook.com>
2022-10-24 16:16:07 +0500

    Lesson 3 - Task 2 - Version 1.1

--- Lesson3/Task2/Select.sql
+++ Lesson3/Task2/Select.sql
@@ -9,10 +9,12 @@ SELECT track FROM tracks
 WHERE duration >= 3.5;
 
 SELECT title FROM collections
-WHERE year_of_release >= 2018 AND year_of_release <= 2020;
+WHERE year_of_release BETWEEN 2018 AND 2020
+GROUP BY title
+ORDER BY title;
 
 SELECT musicians FROM musicians
-WHERE musicians NOT LIKE '%[ ]%';
+WHERE musicians NOT LIKE '%% %';
 
 SELECT track FROM tracks
 WHERE track LIKE '%my%' OR track LIKE '%мой%';
