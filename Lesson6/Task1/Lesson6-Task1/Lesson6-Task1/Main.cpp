864683d69a1ff7d2a7733977db960fcde3eae5ca
Grushaas <grusha-as@yandex.ru>
2022-11-22 17:56:31 +0500

    Lesson 6 - Task 1 - Version 0.1.0

--- Lesson6/Task1/Lesson6-Task1/Lesson6-Task1/Main.cpp
+++ Lesson6/Task1/Lesson6-Task1/Lesson6-Task1/Main.cpp
@@ -2,9 +2,15 @@
 #include <Wt/Dbo/Dbo.h>
 #include <Wt/Dbo/backend/Postgres.h>
 #include <string>
+#include <Windows.h>
 
 using namespace std;
 using namespace Wt::Dbo;
+
+class publisher;
+class book;
+class stock;
+class sale;
 
 class publisher
 {
@@ -16,7 +22,7 @@ public:
 	void persist(Action& a)
 	{
 		field(a, name, "name");
-		hasMany(a, books, ManyToOne, "name");
+		hasMany(a, books, ManyToOne, "id_publisher");
 	}
 };
 
@@ -88,5 +94,80 @@ public:
 
 int main()
 {
-	// Подключение
+	SetConsoleCP(CP_UTF8);
+
+	string answerContinue;
+	try
+	{
+		string connectionString =
+			"host=localhost"
+			" port=5432"
+			" dbname=lesson6_task1"
+			" user=netology"
+			" password=netology2022";
+
+		auto netology = make_unique<backend::Postgres>(connectionString);
+
+		Session session;
+		session.setConnection(move(netology));
+		session.mapClass<publisher>("publisher");
+		session.mapClass<shop>("shop");
+		session.mapClass<book>("book");
+		session.mapClass<stock>("stock");
+		session.mapClass<sale>("sale");
+
+		//session.createTables();
+
+		cout << "Continue?(Yes/No): " << endl;
+		cin >> answerContinue;
+
+		if (answerContinue == "Yes")
+		{
+			Transaction publishTrans{ session };
+			unique_ptr<publisher> newPublisher = make_unique<publisher>();
+
+			newPublisher->name = "Maksim";
+
+			ptr<publisher> publisherPtr = session.add(move(newPublisher));
+			publishTrans.commit();
+
+			Transaction bookTrans{ session };
+			unique_ptr<book> newBook = make_unique<book>();
+
+			newBook->title = "Hello, world!";
+			newBook->publisher = publisherPtr;
+
+			ptr<book> bookPtr = session.add(move(newBook));
+			ptr<shop> shopPtr = session.add(make_unique<shop>());
+			ptr<stock> stockPtr = session.add(make_unique<stock>());
+
+			shopPtr.modify()->name = "BookShelf";
+			stockPtr.modify()->book = bookPtr;
+			stockPtr.modify()->shop = shopPtr;
+			stockPtr.modify()->count = 12;
+
+			bookTrans.commit();
+
+			Transaction salesTrans{ session };
+
+			unique_ptr<sale> newSale = make_unique<sale>();
+
+			newSale->price = 1450;
+			newSale->date_sale = "01.01.2022";
+			newSale->stock = stockPtr;
+			newSale->count = 4;
+
+			ptr<sale> salePtr = session.add(move(newSale));
+
+			salesTrans.commit();
+		}
+		else if (answerContinue == "No")
+		{
+			cout << "Таблицы созданы, выход из программы!" << endl;
+		}
+	}
+	catch (const Exception& e)
+	{
+		cout << e.what() << endl;
+	}
 }
